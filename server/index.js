ae32e95f6153ba1872d3ebbaf9cee7775a2e9f76
MOHD SAZID KHAN <sajidpahat786@gmail.com>
2023-01-04 20:02:17 +0530

    ss

--- server/index.js
+++ server/index.js
@@ -24,15 +24,14 @@ app.use('/api', routes)
 app.use(express.json());
 
 app.use(express.static('public'));
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client', 'build')));
   app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
   })
 }
+
 app.listen(PORT, () => {
   console.log(`API Server is running on ${PORT}`)
 }) 
